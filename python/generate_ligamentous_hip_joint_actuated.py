57c9fab7540a2743b522e970b7fd952b1bc83bf4
Qualot <nakashima@robot.t.u-tokyo.ac.jp>
2026-04-23 23:37:55 +0200

    add socket part by including external mjcf file generated by obj2mjcf

--- python/generate_ligamentous_hip_joint_actuated.py
+++ python/generate_ligamentous_hip_joint_actuated.py
@@ -44,6 +44,8 @@ class LigamentousHipBuilder:
         # Build hierarchy
         pelvis_frame = self._add_pelvis_frame()
         base_plate = self._add_base_plate(pelvis_frame)
+        self._add_convex_socket(base_plate)
+
         sites_origin_body = self._add_origin_container(base_plate)
         
         link = self._add_link_parts()
@@ -137,6 +139,28 @@ class LigamentousHipBuilder:
                 rgba=[0, 1, 0, 1] if i != 0 else [0, 0, 1, 1]
             )
             self.relay_sites.append(s_relay)
+
+    def _add_convex_socket(self, parent_body):
+        """Load an external MJCF socket and attach it to the parent body"""
+        # 1. Load the external spec
+        socket_file = "../assets/socket_0/socket_0.xml"
+        socket_spec = mujoco.MjSpec.from_file(socket_file)
+        
+        # 2. Get the root body from the external spec
+        # (Usually socket_spec.worldbody.bodies[0] is the body defined in the XML)
+        # socket_source = socket_spec.worldbody.bodies[0]
+        socket_source = socket_spec.body('socket_0')  # Assuming the body in the XML is named 'socket_0'
+        socket_pos = parent_body.add_frame(name="socket_frame", pos=[0, 0, -0.015], euler=[180, 0, 0])
+        
+        # 3. Create a new body in our current spec and copy EVERYTHING from the source
+        # This will copy geoms, child bodies, sites, etc.
+        #new_socket_body = parent_body.attach_body(socket_source, 'socket', '')
+        socket_pos.attach_body(socket_source, 'socket', '')  # Attach the new body to the frame for correct positioning
+        
+        # Optional: If you want to change its position after copying
+        # new_socket_body.pos = [0, 0, 0] 
+        
+        return socket_pos
 
     def _generate_ligaments(self, origin_body, link_body):
         """Create sites and ligaments using pre-generated relay sites"""
