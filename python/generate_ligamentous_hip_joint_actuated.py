825abcf236983efa679ab3d30bafc150c877381c
Qualot <nakashima@robot.t.u-tokyo.ac.jp>
2026-05-08 15:17:48 +0200

    elongate inclined link long cylinder a bit for appearance

--- python/generate_ligamentous_hip_joint_actuated.py
+++ python/generate_ligamentous_hip_joint_actuated.py
@@ -251,7 +251,7 @@ class LigamentousHipBuilder:
 
         link_stick = link_inclined.add_body(name="link_stick", pos=[0, 0, -self.config.ball_diameter], euler=[-50, 0, 0])
         link_stick.add_geom(name="long_cylinder", type=mujoco.mjtGeom.mjGEOM_CYLINDER, 
-                      fromto=[0, 0, 0, 0, 0, -0.3], size=[0.015], rgba=[0.7, 0.7, 0.7, 1])
+                      fromto=[0, 0, 0.01, 0, 0, -0.3], size=[0.015], rgba=[0.7, 0.7, 0.7, 1])
         link_stick.add_geom(name="weight", type=mujoco.mjtGeom.mjGEOM_SPHERE, 
                       pos=[0, 0, -0.3], size=[0.08], mass=5, rgba=[.2, .2, .2, 1])
 
